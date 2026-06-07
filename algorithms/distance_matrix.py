50e24cb58a1c7da716135659917e00a12bf64360
dat2912005lqd-collab <dat2912005lqd@gmail.com>
2026-06-07 22:15:02 +0700

    Update distance_matrix.py

---
+++ algorithms/distance_matrix.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import math
+import heapq
+class DistanceMatrix:
+  def __init__(self,traffic_sschedule_path,shipper_path):
+    
+    pass
+  def load_traffic_data(self,filepath):
+    
+    pass
+  def build_node_list(self):
+    pass 
+  def build_traffic_graph(self):
+    pass
+  def compute_distance_matrix(self):
+    pass
+  def get_travel_time(self, from_idx,to_idx):
+    pass
