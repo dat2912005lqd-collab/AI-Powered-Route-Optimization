5f6894cda35ba79618cd8940a9fc63d845a0c3f0
dat2912005lqd-collab <dat2912005lqd@gmail.com>
2026-06-07 21:14:00 +0700

    Hoàn thành file astar.py

--- algorithms/astar.py
+++ algorithms/astar.py
@@ -30,7 +30,41 @@ class AStarPlanner:
           current=current.parent
         return path[::-1]
       current_coords=(current.latitude, current.longitude)
-      for next 
+      for next_coords,travel_cost in self.traffic_graph.get(current_coords,[]):
+        new_g=current.g+travel_cost
+        if next_coords not in visited_g or new_g< visited_g[next_coords]:
+          visited_g[next_coords]=new_g 
+          neighbor=Node(next_coords[0],next_coords[1],g=new_g,parent=current)
+          neighor.h=self.heuristic(neighbor,goal_node)
+          neighbor.f=neighbor.g+neighbor.h
+          heapq.heappush(open_set,negihbor)
+    return []
+def load_graph_from_csv(flie_path):
+  points=[]
+  with open(file_path,mode='r',encoding='utf-8') as f:
+    reader=csv.DictReader(f)
+    for row in reader:
+      latitude=float(row['latitude'])
+      longitude=float(row['longitude'])
+      point.append((latitude,longitude))
+  graph={}
+  for p1 in points:
+    graph[p1]=[]
+    for p2 in points:
+      if p1!=p2:
+        dist=math.hypot(p1[0]-p2[0],p1[1]-p2[1])*111
+        graph[p1].append((p2,dist))
+  return graph
+if __name__ =='__main__':
+  traffic_graph=load_graph_from_csv('vietnam_cities.csv')
+  planner=AStarPlanner(traffic_graph) 
+  start = (21.0285, 105.8542) 
+  goal = (10.7769, 106.7009)  
+  route = planner.search(start, goal)
+  print(f"Lộ trình tối ưu qua các thành phố: {route}")
+  
+    
+        
           
     
     
