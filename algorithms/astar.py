394061ac25a2c7004289e2f64e2f1752ebf73278
dat2912005lqd-collab <dat2912005lqd@gmail.com>
2026-06-07 20:02:11 +0700

    Update astar.py

--- algorithms/astar.py
+++ algorithms/astar.py
@@ -2,4 +2,15 @@ import math
 import heapq
 class Node:
   def __init__(latitude, longitude,g,h,parent=Node):
+    self.latitude=latitude
+    self.longitude=longitude
+    self.g, self.h=g,h
+    self.f=g+h
+    self.parent=parent
+  def __lt__(self,other):
+    return self.f<other.f
+class AStarPlanner:
+  def __init__(self,traffic_graph):
+    slelf
+    
     
