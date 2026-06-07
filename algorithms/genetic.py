f787e633e1247fc763d4e01a84585c1b6fd67107
dat2912005lqd-collab <dat2912005lqd@gmail.com>
2026-06-07 22:24:51 +0700

    Update genetic.py

---
+++ algorithms/genetic.py
@@ -0,0 +1,21 @@
+import random
+import numpy as np
+class GeneticTSP:
+  def __init__(self,distance_matrix,time_windows,depot_idx=0):
+    pass
+  def random_route(self):
+    pass
+  def _route_time(self,route):
+    pass
+  def _fitness(self,route):
+    pass
+  def _selection(self):
+    pass
+  def _crossover(self,parent1,parent2):
+    pass
+  def _mutate(self,route):
+    pass
+  def iinitialize_demand(self,demand):
+    pass
+  def evolve(self,
+  
