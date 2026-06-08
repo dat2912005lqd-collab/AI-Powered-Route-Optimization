14810d25541107dc516642ddda9bc4f695ba6392
dat2912005lqd-collab <dat2912005lqd@gmail.com>
2026-06-08 08:24:18 +0700

    Hoàn thiện file tính toán khoảng cách tối ưu đường bộ

--- algorithms/distance_matrix.py
+++ algorithms/distance_matrix.py
@@ -2,19 +2,21 @@ import pandas as pd
 import numpy as np
 from math import radians,cos, sin,asin,sqrt
 class DistanceMatrixBuilder:
-  def __init__(self,cities_name,traffic_path,shipper_path):
-    self.df_cities=pd.read_csv(cities_name)
+  def __init__(self,cities_path,traffic_path,shipper_path):
+    self.df_cities=pd.read_csv(cities_path)
     self.df_traffic=pd.read_csv(traffic_path)
     self.df_shipper=pd.read_csv(shipper_path)
-  def haversine_distance(self,lon1,lat1,lon2,lat2):
-    lon1, lat1,lon2,lat2=map(radians,[lon1, lat1, lon2,lat2])
-    dlon=lon2-lon1
-    dlat=lat2-lat1
-    a=sin(dlat/2)**2+cos(lat1)*cos(lat2)* sin(dlon/2)**2
-    c=2*asin(sqrt(a))
-    r=6731
-    return c*r
-  def get_traffic_factor(self,time_slot):
-    traffic_row=self.df_trafic[self.df_traffic['time_slot']==time_slot]
-    if not traffic_row.empty:
-      return traffic_row['congestion_factor'].values[0]
+ def build_time_matrix(self,hour,day_type,shipper_id):
+   t_row=self.traffic[(self_traffic['hour']==hour)&(self.traffic['day_type']==day_type)]
+   traffic_f=t_row['traffic_factor'].values[0]
+   s_row=self.shipper[self_shipper['shipper_id']==shipper_id]
+   shipper_f=s_row['shipper'].values[0]
+   v_effective=(40/60)*shipper_f/traffic_f
+   lat=np.radians(self.cities['latitude'].values)
+   lon=np.radians(self.cities['longitude'].values)
+   dlat=lat[:,None]-lat
+   dlon=lon[:,None]-lon
+   a=np.sin(dlat/2)**2+np.cos(lat[:,None])*np.cos(lat)*np.sín(dlon/2)**2
+   distance_matrix=2*6371*np.arcsin(np.sqrt(a))*1.3
+   time_matrix=distance_matrix/v_effective
+   return time_matrix
