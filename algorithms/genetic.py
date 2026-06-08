c766c738c0e77379223e3c2a88225c45a1f368d5
dat2912005lqd-collab <dat2912005lqd@gmail.com>
2026-06-08 08:52:09 +0700

    Refactor GeneticTSP to GeneticRoutingSolver with new methods

--- algorithms/genetic.py
+++ algorithms/genetic.py
@@ -1,21 +1,63 @@
 import random
 import numpy as np
-class GeneticTSP:
-  def __init__(self,distance_matrix,time_windows,depot_idx=0):
-    pass
-  def random_route(self):
-    pass
-  def _route_time(self,route):
-    pass
-  def _fitness(self,route):
-    pass
-  def _selection(self):
-    pass
-  def _crossover(self,parent1,parent2):
-    pass
-  def _mutate(self,route):
-    pass
-  def iinitialize_demand(self,demand):
-    pass
-  def evolve(self,
+class GeneticRouting Solver:
+def __init__(self,time_matrix,demands,capacity, pop_size=100,generations=200,mutate_rates=0,1):
+  self.time_matrix=time_matrix
+  self.demands=demands
+  self.capacity=capacity
+  self.pop_size=pop_size
+  self.generations=generations
+  self.mutate_rates=mutate_rates
+  self.num_cities=len(demands)
+def _generate_cchromosome(self):
+  cities=list(range(1,self.num_cities))
+  random.shuffle(cities)
+  return cities
+def calculate_fitness(self,chromosome):
+  total_time=0
+  current_load=0
+  prev_node=0
+  for node in chromosome:
+    if current_load+self.demands[node]>self.capacity:
+      total_time+=self.time_matrix[prev_node][0]+self.time_matrix[0][node] 
+      current_load=self.demamds[node]
+    else:
+      total_time+=self.time_matrix[prev_node][node]
+      current_load+=self.demands[node]
+    prev_node=node
+  total_time+= self.time_matrix[prev_node][0]
+  return 1/total_time
+def evolve(self):
+  popurt random
+import numpy as np
+class GeneticRouting Solver:
+def __init__(self,time_matrix,demands,capacity, pop_size=100,generations=200,mutate_rates=0,1):
+  self.time_matrix=time_matrix
+  self.demands=demands
+  self.capacity=capacity
+  self.pop_size=pop_size
+  self.generations=generations
+  self.mutate_rates=mutate_rates
+  self.num_cities=len(demands)
+def _generate_cchromosome(self):
+  cities=list(range(1,self.num_cities))
+  random.shuffle(cities)
+  return cities
+def calculate_fitness(self,chromosome):
+  total_time=0
+  current_load=0
+  prev_node=0
+  for node in chromosome:
+    if current_load+self.demands[node]>self.capacity:
+      total_time+=self.time_matrix[prev_node][0]+self.time_matrix[0][node] 
+      current_load=self.demamds[node]
+    else:
+      total_time+=self.time_matrix[prev_node][node]
+      current_load+=self.demands[node]
+    prev_node=node
+  total_time+= self.time_matrix[prev_node][0]
+  return 1/total_time
+def evolve(self):
+  popu
   
+      
