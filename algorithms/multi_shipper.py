4f60ad1160591b05d2dcc3fd3e5ca25b1fad3608
dat2912005lqd-collab <dat2912005lqd@gmail.com>
2026-06-08 10:49:36 +0700

    Hoàn thiện file quản lý tối ưu hóa nhiều shipper cùng 1 lúc

--- algorithms/multi_shipper.py
+++ algorithms/multi_shipper.py
@@ -7,4 +7,45 @@ class MultiShipperCoordinator:
     self.matrix_builder=DistanceMatrixBuilder(cities_path,traffic_path,shipper_path)
     self.df_shipper=pd.read_csv(shipper_path)
     self.df_cities=pd.read_csv(cities_path)
-  def 
+  def _assign_cities_to_shippers(self,active_shippers):
+    shipper_tasks={s['shipper_id']: [] for _, s in active_shippers .iterrows()}
+    delivery_points=self.df_cities[self.df_cities['city_id']!=0].to_dict('records')
+    for point in delivery_points:
+      best_shipper_id=None
+      min_dist=float('inf')
+      for _,shipper in active_shippers.iterrows():
+        shipper_home=self.df_cities[self.df_cities['city_id']==shipper['city_id']].iloc[0]
+        dist=self.matrix_builder.haversine_distance(point['longitude' ],point['latitude'],shipper_home['longitude'],shipper_home['latitude'])
+        if dist< min_dist:
+          min_dist=dist
+          best_shipper_id=shipper['shipper_id']
+      if best_shipper_id:
+        shipper_tasks[best_shipper_id].append(point)
+    return shipper_tasks
+  def run_multi_routing(self,hour,day_type):
+    active_shippers=self.df_shipper[self.df_shipper['status']=='active']
+    shipper_tasks=self._assign_cities_to_shippers(active_shippers)
+    final_routes={}
+    for _,shipper in active_shippers.iterrows():
+      s_id=shipper['shipper_id']
+      assigned_points=shipper_tasks[s_id]
+      if not assigned_points:
+        continue
+      shipper_home=self.df_cities[self.df_cities['city_id']==shipper['city_id']].iloc[0].to_dict()
+      local_cities=[shipper_home]+assigned_points
+      df_local_cities=pd.DataFrame(local_cities)
+      self.matrix_builder.cities=df_local_cities
+      time_matrix=self.matrix_builder.build_time_matrix(hour,day_type,s_id)
+      demands=df_local_cities['demand'].values
+      solver=GeneticRoutingSolver(time_matrix=time_matrix, demands=demands,capacity=shipper['capacity']
+      best_chrom,total_time=solver.evolve()
+       route_names = [df_local_cities.iloc[0]['city_name']] + [df_local_cities.iloc[i]['city_name'] for i in best_chrom]
+            final_routes[s_id] = {
+                "route": route_names,
+                "total_time_minutes": round(total_time, 2)
+            }
+        return final_routes
+      
+    
+      
+          
